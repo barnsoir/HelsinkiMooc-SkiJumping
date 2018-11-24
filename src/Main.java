2262083bafdeeee2fdda53b90459f2188ad095dc
barnsoir <barney.lewis@gmail.com>
2018-11-24 16:26:28 +0000

    Updated UI to reflect website

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-        
+
 public class Main {
 
     public static void main(String[] args) {
         // Write your main program here. Implementing your own classes will be very useful.
-        Scanner reader = new Scanner(System.in);
-        
+
         TournamentUserInterface tournamentUI = new TournamentUserInterface();
+        tournamentUI.start();
     }
 }
--- src/TournamentUserInterface.java
+++ src/TournamentUserInterface.java
@@ -8,6 +8,62 @@
  *
  * @author barne
  */
+import java.util.Scanner;
+
 public class TournamentUserInterface {
-    
+
+        Scanner reader = new Scanner(System.in);
+
+        public void start() {
+
+            System.out.println("Kumpula ski jumping week");
+            System.out.println();
+            enterParticipants();
+            enterRounds();
+            displayResults();
+        }
+
+        public void enterParticipants() {
+
+            System.out.println("Write the names of the participants one at a time; an empty string brings you to the jumping phase.");
+
+            while (true) {
+                String input = getParticipant();
+
+                if (!input.equals("")) {
+                    // add participant
+                } else {
+                    System.out.println("The tournament begins!");
+                    break;
+                }
+            }
+        }
+
+        public String getParticipant() {
+            System.out.print("Participant name: ");
+            return this.reader.nextLine();
+        }
+
+        public void enterRounds() {
+            while(true) {
+                String input = getRound();
+
+                if (input.equals("jump")) {
+                    // new round
+                } else {
+                    System.out.println("Thanks!");
+                    break;
+                }
+            }
+        }
+
+        public String getRound() {
+            System.out.print("Write \"jump\" to jump; otherwise you quit: ");
+
+            return this.reader.nextLine();
+        }
+
+        public void displayResults() {
+            System.out.println("Tournament results:");
+        }
 }
