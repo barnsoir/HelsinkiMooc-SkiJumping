6d68dfe977accd22056338c36fab23242f199764
barnsoir <barney.lewis@gmail.com>
2018-11-29 20:19:06 +0000

    Added base jumper and tournament class

---
+++ src/Jumper.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author barne
+ */
+public class Jumper implements Comparable<Jumper> {
+    
+    private String name;
+    private int score;
+    
+    public Jumper(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+    
+    public int getScore () {
+        return this.score;
+    }
+    
+    public void addRoundScore (int roundScore) {
+        this.score += roundScore;
+    }
+    
+    public int compareTo(Jumper jumper) {
+        if (this.score == jumper.getScore()) {
+            return 0;
+        } else if (this.score > jumper.getScore()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/Tournament.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author barne
+ */
+import java.util.*;
+
+public class Tournament {
+    
+    private ArrayList<Jumper> jumpers;
+    
+    public Tournament() {
+       this.jumpers = new ArrayList<Jumper>();
+    }
+    
+    public void addParticipant(Jumper jumper) {
+        this.jumpers.add(jumper);
+    }
+}
