6055ebd9cb8baf2303604a990f7d53542d97a2de
barnsoir <barney.lewis@gmail.com>
2018-11-30 18:19:37 +0000

    Implemented Jump Class

---
+++ src/Jump.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Jump {
+
+    private List<Integer> votes;
+    private int length;
+
+    public Jump() {
+        this.length = generateLength();
+        this.votes = new ArrayList<Integer>();
+        generateVotes(5, votes);
+    }
+
+    public int generateLength() {
+        return (int)(60 + Math.random()*60);
+    }
+
+    public void generateVotes(int number, List<Integer> votes) {
+        for (int i=0; i<number; i++) {
+            votes.add((int)(10 + Math.random()*10));
+        }
+    }
+
+    public void printJump() {
+        System.out.println("    length: " + this.length);
+        System.out.println("    judge votes: [");
+        for (int j=0; j<votes.size(); j++) {
+            System.out.print(votes.get(j));
+            if (j<(votes.size()-1)) {
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
+    }
+}
